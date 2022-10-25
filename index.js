bea3ba75006ebce6b45a55ff516488b46a4dd8ce
littyofficials@gmail.com <BryteLitty>
2022-10-25 01:20:22 -0700

    add background image to text

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+// Featured Speakers Data
+const data = [
+    {
+        name: "Bryte Litty Tricqx",
+        title: "Berkam School of Law",
+        bio: "This niqqa really understand what it takes to implement systems thinking",
+        image: "./assets/images/boy.png"
+    }
+]
+
 // mobile menu selectors
 const openBtn = document.querySelector('.nav-icon');
 const closeBtn = document.querySelector('.close-btn');
